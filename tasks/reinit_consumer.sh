3aff3eb96aa6fccdd09209f4c1ac2fc036ea8c58
Paul Bailey <paulba@stedwards.edu>
2018-01-18 14:42:25 -0600

    update task for starttls

--- tasks/reinit_consumer.sh
+++ tasks/reinit_consumer.sh
@@ -7,4 +7,11 @@ replace: nsds5BeginReplicaRefresh
 nsds5BeginReplicaRefresh: start
 EOM
 
-echo "$REINIT" | ldapmodify -h $PT_server_host -p $PT_server_port -x -D "${PT_root_dn}" -w $PT_root_dn_pass
+if [ $PT_starttls ]
+then
+  LM_OPTS='ZxH'
+else
+  LM_OPTS='xH'
+fi
+
+echo "$REINIT" | ldapmodify -$LM_OPTS $PT_protocol://$PT_server_host:$PT_server_port -x -D "${PT_root_dn}" -w $PT_root_dn_pass
